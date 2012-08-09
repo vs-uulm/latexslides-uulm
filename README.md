04d64847c084c77d9a21446b8a34210fc6219ec3
Stefan Dietzel <mail@dietzel.st>
2012-08-09 09:34:18 +0200

    Initial commit of uulm slides.

---
+++ README.md
@@ -0,0 +1,12 @@
+UUlm Latex Beamer Template
+==========================
+
+A document class that can be used to create LaTeX `beamer` slides featuring (a slight variation of) the UUlm corporate design.
+
+Look at the `example` folder for a working example.
+
+
+Installation
+------------
+
+The script `install.sh` can be used to install the package into the local `texmf` tree. Alternatively, you can check out the git repository directly into the `texmf` tree.
---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+PACKAGE_NAME="uulm"
+
+function copy_files() {
+	for type in cls pdf; do
+		for f in *.${type}; do
+			echo " * $f"
+			cp $f $1
+		done
+	done
+}
+
+if [ "$1" == "" ]; then
+	echo "Usage: $0 <install-path>"
+	echo "Suitable paths seem to be:"
+	kpsepath tex |tr : "\n" |egrep "$HOME.*/tex///" |
+		sed 's/\/\///' |
+		sed 's/^/ * /'
+	exit 1
+fi
+
+DIR=$1
+
+CORRECT=0
+kpsepath tex | grep $DIR >/dev/null && CORRECT=1
+
+if [ "$CORRECT" -eq "0" ]; then
+	echo "$DIR is not a correct install path."
+	exit 2
+fi
+
+if [ -d "$DIR/latex/$PACKAGE_NAME" ]; then
+	echo "Old version found. Replacing files:"
+	copy_files $DIR/latex/$PACKAGE_NAME
+else
+	echo "Initial installation. Copying files:"
+	mkdir $DIR/latex/$PACKAGE_NAME
+	copy_files $DIR/latex/$PACKAGE_NAME
+fi
+
+echo "Testing installation:"
+
+OLDPWD="$PWD"
+for cls in *.cls; do
+	cd /
+	kpsewhich $cls || echo $cls ... NOT FOUND
+done
+cd $OLDPWD
